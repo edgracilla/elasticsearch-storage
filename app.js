94b837b4f6aa9262dac4bc325863caa257075728
edgracilla <ed.gracilla@gmail.com>
2017-02-22 14:32:25 +0800

    v3 compatible

--- app.js
+++ app.js
@@ -1,96 +1,84 @@
-'use strict';
+'use strict'
 
-var async         = require('async'),
-	platform      = require('./platform'),
-	isPlainObject = require('lodash.isplainobject'),
-	isArray       = require('lodash.isarray'),
-	client, opt;
+const reekoh = require('reekoh')
+const _plugin = new reekoh.plugins.Storage()
 
-let sendData = function (data, callback) {
-	var createObject = {
-		index: opt.index,
-		type: opt.type
-	};
+const async = require('async')
+const elasticsearch = require('elasticsearch')
+const isPlainObject = require('lodash.isplainobject')
 
-	if (opt.id_field) {
-		createObject.id = data[opt.id_field];
-		delete data[opt.id_field];
-	}
+let client = null
+let options = null
 
-	if (opt.parent_field) {
-		createObject.parent = data[opt.parent_field];
-		delete data[opt.parent_field];
-	}
+let sendData = (data, callback) => {
+  let createObject = {
+    index: options.index,
+    type: options.type
+  }
 
-	client.create(Object.assign(createObject, {
-		body: data
-	}), (error, result) => {
-		if (!error) {
-			platform.log(JSON.stringify({
-				title: 'Record Successfully inserted to ElasticSearch.',
-				data: result
-			}));
-		}
+  if (options.idField) {
+    createObject.id = data[options.idField]
+    delete data[options.idField]
+  }
 
-		callback(error);
-	});
-};
+  if (options.parentField) {
+    createObject.parent = data[options.parentField]
+    delete data[options.parentField]
+  }
 
-platform.on('data', function (data) {
-	if (isPlainObject(data)) {
-		sendData(data, (error) => {
-			if (error) platform.handleException(error);
-		});
-	}
-	else if (isArray(data)) {
-		async.each(data, (datum, done) => {
-			sendData(datum, done);
-		}, (error) => {
-			if (error) platform.handleException(error);
-		});
-	}
-	else
-		platform.handleException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`));
-});
+  client.create(Object.assign(createObject, {
+    body: data
+  }), (error, result) => {
+    if (!error) {
+      process.send({ type: 'processed' })
+      _plugin.log(JSON.stringify({
+        title: 'Record Successfully inserted to ElasticSearch.',
+        data: result
+      }))
+    }
 
-/*
- * Event to listen to in order to gracefully release all resources bound to this service.
- */
-platform.on('close', function () {
-	platform.notifyClose();
-});
+    callback(error)
+  })
+}
 
-/*
- * Listen for the ready event.
- */
-platform.once('ready', function (options) {
-	let host          = `${options.host}`,
-		config        = require('./config.json'),
-		elasticsearch = require('elasticsearch'),
-		auth          = '',
-		apiVersion;
+_plugin.on('data', (data) => {
+  if (isPlainObject(data)) {
+    sendData(data, (error) => {
+      if (error) _plugin.logException(error)
+    })
+  } else if (Array.isArray(data)) {
+    async.each(data, (datum, done) => {
+      sendData(datum, done)
+    }, (error) => {
+      if (error) _plugin.logException(error)
+    })
+  } else {
+    _plugin.logException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`))
+  }
+})
 
-	opt = options;
+_plugin.once('ready', () => {
+  options = _plugin.config
 
-	if (options.port)
-		host = `${host}:${options.port}`;
+  let host = options.port
+    ? `${options.host}:${options.port}`
+    : options.host
 
-	if (options.user)
-		auth = `${options.user}`;
+  let auth = options.user
+    ? options.user
+    : ''
 
-	if (options.password)
-		auth = `${auth}:${options.password}@`;
-	else if (options.user)
-		auth = `${auth}:@`;
+  auth = options.password
+    ? `${auth}:${options.password}@`
+    : `${auth}:@`
 
-	apiVersion = options.apiVersion || config.apiVersion.default;
-	host = `${options.protocol}://${auth}${host}`;
+  host = `${options.protocol}://${auth}${host}`
 
-	client = new elasticsearch.Client({
-		host: host,
-		apiVersion: apiVersion
-	});
+  client = new elasticsearch.Client({
+    host: host,
+    apiVersion: options.apiVersion
+  })
 
-	platform.log('ElasticSearch Storage plugin initialized.');
-	platform.notifyReady();
-});
+  _plugin.log('ElasticSearch Storage plugin initialized.')
+  process.send({ type: 'ready' })
+})
--- test/elasticsearch-storage.test.js
+++ test/elasticsearch-storage.test.js
@@ -1,119 +1,127 @@
-'use strict';
+/* global describe, it, before, after */
 
-const HOST     = 'reekoh.east-us.azr.facetflow.io',
-	  USER     = 'KD8xosnl80e2WR8eyfqENgoNo4VUOs0S',
-	  PROTOCOL = 'https',
-	  INDEX    = 'reekoh_index',
-	  TYPE     = 'reekoh_type',
-	  _ID      = new Date().getTime();
+'use strict'
 
-var cp     = require('child_process'),
-	assert = require('assert'),
-	should = require('should'),
-	moment = require('moment'),
-	storage;
+const amqp = require('amqplib')
+const should = require('should')
+const cp = require('child_process')
 
-var record = {
-	_id: _ID,
-	co2: '11%',
-	temp: 23,
-	quality: 11.25,
-	reading_time: '2015-11-27T11:04:13.539Z',
-	metadata: '{"metadata_json": "reekoh metadata json"}',
-	random_data: 'abcdefg',
-	is_normal: true
-};
+const INPUT_PIPE = 'demo.pipe.storage'
+const BROKER = 'amqp://guest:guest@127.0.0.1/'
+const _ID = new Date().getTime()
+
+let _app = null
+let _conn = null
+let _channel = null
+
+let conf = {
+  host: 'localhost',
+  port: 9200,
+  user: '',
+  protocol: 'http',
+  idField: '_id',
+  index: 'reekon_index',
+  type: 'reekoh_type',
+  apiVersion: '2.1'
+}
+
+let record = {
+  _id: _ID,
+  co2: '11%',
+  temp: 23,
+  quality: 11.25,
+  readingTime: '2015-11-27T11:04:13.539Z',
+  metadata: '{"metadataJson": "reekoh metadata json"}',
+  randomData: 'abcdefg',
+  isNormal: true
+}
 
 describe('Storage', function () {
-	this.slow(5000);
+  this.slow(5000)
 
-	after('terminate child process', function () {
-		storage.send({
-			type: 'close'
-		});
+  before('init', () => {
+    process.env.BROKER = BROKER
+    process.env.INPUT_PIPE = INPUT_PIPE
+    process.env.CONFIG = JSON.stringify(conf)
 
-		setTimeout(function () {
-			storage.kill('SIGKILL');
-		}, 3000);
-	});
+    amqp.connect(BROKER).then((conn) => {
+      _conn = conn
+      return conn.createChannel()
+    }).then((channel) => {
+      _channel = channel
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
 
-	describe('#spawn', function () {
-		it('should spawn a child process', function () {
-			assert.ok(storage = cp.fork(process.cwd()), 'Child process not spawned.');
-		});
-	});
+  after('terminate child process', function () {
+    _conn.close()
+    setTimeout(function () {
+      _app.kill('SIGKILL')
+    }, 3000)
+  })
 
-	describe('#handShake', function () {
-		it('should notify the parent process when ready within 5 seconds', function (done) {
-			this.timeout(5000);
+  describe('#spawn', function () {
+    it('should spawn a child process', function () {
+      should.ok(_app = cp.fork(process.cwd()), 'Child process not spawned.')
+    })
+  })
 
-			storage.on('message', function (message) {
-				if (message.type === 'ready')
-					done();
-			});
+  describe('#handShake', function () {
+    it('should notify the parent process when ready within 5 seconds', function (done) {
+      this.timeout(5000)
 
-			storage.send({
-				type: 'ready',
-				data: {
-					options: {
-						host: HOST,
-						user: USER,
-						protocol: PROTOCOL,
-						id_field: '_id',
-						index: INDEX,
-						type: TYPE
-					}
-				}
-			}, function (error) {
-				assert.ifError(error);
-			});
-		});
-	});
+      _app.on('message', (message) => {
+        if (message.type === 'ready') {
+          done()
+        }
+      })
+    })
+  })
 
-	describe('#data', function () {
-		it('should process the data', function (done) {
+  describe('#data', function () {
+    it('should process the data', function (done) {
+      this.timeout(8000)
+      _channel.sendToQueue(INPUT_PIPE, new Buffer(JSON.stringify(record)))
 
-			storage.send({
-				type: 'data',
-				data: record
-			}, done);
+      _app.on('message', (msg) => {
+        if (msg.type === 'processed') { done() }
+      })
+    })
+  })
 
-		});
-	});
+  describe('#data', function () {
+    it('should have inserted the data', function (done) {
+      this.timeout(5000)
 
-	describe('#data', function () {
-		it('should have inserted the data', function (done) {
-			this.timeout(5000);
+      let elasticsearch = require('elasticsearch')
+      // let url = conf.protocol + '://' + conf.user + ':@' + conf.host
+      let url = 'http://localhost:9200/'
 
-			var elasticsearch = require('elasticsearch');
+      let client = new elasticsearch.Client({
+        host: url,
+        apiVersion: '1.0'
+      })
 
-			var url = PROTOCOL + '://' + USER + ':@' + HOST;
+      client.get({
+        index: conf.index,
+        type: conf.type,
+        id: _ID
+      }, function (error, response) {
+        if (error) return console.log(error)
+        response.should.have.property('_source')
 
-			var client = new elasticsearch.Client({
-				host: url,
-				apiVersion: '1.0'
-			});
+        let resp = response._source
 
-			client.get({
-				index: INDEX,
-				type: TYPE,
-				id: _ID
-			}, function (error, response) {
+        should.equal(record.co2, resp.co2, 'Data validation failed. Field: co2')
+        should.equal(record.temp, resp.temp, 'Data validation failed. Field: temp')
+        should.equal(record.quality, resp.quality, 'Data validation failed. Field: quality')
+        should.equal(record.randomData, resp.randomData, 'Data validation failed. Field: randomData')
+        should.equal(record.readingTime, resp.readingTime, 'Data validation failed. Field: readingTime')
+        should.equal(record.metadata, resp.metadata, 'Data validation failed. Field: metadata')
 
-				response.should.have.property('_source');
-
-				var resp = response._source;
-
-				should.equal(record.co2, resp.co2, 'Data validation failed. Field: co2');
-				should.equal(record.temp, resp.temp, 'Data validation failed. Field: temp');
-				should.equal(record.quality, resp.quality, 'Data validation failed. Field: quality');
-				should.equal(record.random_data, resp.random_data, 'Data validation failed. Field: random_data');
-				should.equal(record.reading_time, resp.reading_time, 'Data validation failed. Field: reading_time');
-				should.equal(record.metadata, resp.metadata, 'Data validation failed. Field: metadata');
-
-				done();
-			});
-		});
-	});
-
-});
+        done()
+      })
+    })
+  })
+})
